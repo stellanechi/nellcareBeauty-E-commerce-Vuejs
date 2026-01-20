81983a6d8ff81d5631444312b091e31f1064b33f
stellanechi <stellanechi65@gmail.com>
2026-01-20 12:58:44 +0100

    fixed catalog drop down issue that wasnt displaying

--- src/components/navbar/Catalog.vue
+++ src/components/navbar/Catalog.vue
@@ -244,7 +244,7 @@
                     class="w-32 h-32 md:w-55 md:h-55 mx-auto bg-white rounded-lg flex items-center justify-center mb-4"
                   >
                     <img
-                      src="https://nellcareBeauty-demo.myshopify.com/cdn/shop/products/per1_360x.jpg?v=1551249474"
+                      src="https://lezada-demo.myshopify.com/cdn/shop/products/per1_360x.jpg?v=1551249474"
                       alt="Organic Moon Fruit"
                       class="w-full h-full object-contain"
                     />
@@ -268,7 +268,10 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const showCatalogMenu = ref(false);
+</script>
 
 <style scoped>
 .fade-enter-active,
